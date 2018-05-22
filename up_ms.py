626054a01b2bbec923173d2b4091008df5630880
JohnSalas8 <johnsalaslp@gmail.com>
2018-05-22 08:50:44 -0500

    Regresion Lineal Multiple

---
+++ up_ms.py
@@ -0,0 +1,13 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+from Services.Multiple import Multiple
+
+#app = Flask(__name__)
+#api = Api(app)
+
+#api.add_resource(Multiple(), '/regresion_lineal_multiple')
+
+
+if __name__ == '__main__':
+    #app.run(port=2409)
+    
