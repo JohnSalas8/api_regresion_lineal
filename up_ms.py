cc1c68b9bc55f6ac233ee757c2af52e2183c0aac
JohnSalas8 <johnsalaslp@gmail.com>
2018-05-22 10:07:06 -0500

    Agregando la api de regresion polynomial

--- up_ms.py
+++ up_ms.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
 from Services.Multiple import Multiple
+from Services.Polynomial import Polynomial
 
 app = Flask(__name__)
 api = Api(app)
@@ -8,15 +9,27 @@ api = Api(app)
 class api_rmultiple(Resource):
     def get(self, x1, x2, y):
         return Multiple().get_result(
-            x1.split(','),
-            x2.split(','),
-            y.split(',')
+            map( float, x1.split(',') ),
+            map( float, x2.split(',') ),
+            map( float, y.split(',') )
+        )
+
+class api_rpolynomial(Resource):
+    def get(self, x, y):
+        return Polynomial().get_result(
+            map(float, x.split(',')),
+            map(float, y.split(','))
         )
         
 
 api.add_resource(
     api_rmultiple, 
     '/rlm/<x1>/<x2>/<y>'
+)
+
+api.add_resource(
+    api_rpolynomial,
+    '/rp/<x>/<y>'
 )
 
 
