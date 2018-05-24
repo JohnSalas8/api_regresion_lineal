5e6935cddff20b4812419d54bbb340d521e380ca
JohnSalas8 <johnsalaslp@gmail.com>
2018-05-24 13:00:10 -0500

    Mejorando servicio

--- up_ms.py
+++ up_ms.py
@@ -6,6 +6,7 @@ from Services.Polynomial import Polynomial
 app = Flask(__name__)
 api = Api(app)
 
+# http://localhost:2409/rlm/0,2,2.5,1,4,7/0,1,2,3,6,2/5,10,9,0,3,27
 @app.route('/rlm/<x1>/<x2>/<y>')
 def APIRmultiple(parameter_list):
     return Multiple().get_result(
@@ -14,15 +15,21 @@ def APIRmultiple(parameter_list):
             map( float, y.split(',') )
         )
 
-@app.route('/rp/<x>/<y>')
+# http://localhost:2409/rlp/0,1,2,3,4,5/2.1,7.7,13.6,27.2,40.9,61.1
+@app.route('/rlp/<x>/<y>')
 def APIRpolynomial(x, y):
     return Polynomial().get_result(
             map(float, x.split(',')),
             map(float, y.split(','))
         )
 
+# http://localhost/rls/
+@app.route('/rls/<x>/<y>')
+def APIRsimple(x, y):
+    pass
+
 @app.route('/')
-def root():
+def index():
     return render_template('information.html')
 
 
